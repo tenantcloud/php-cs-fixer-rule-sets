98b6c88f0dba270368e638ad7c94d41f818163b2
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2021-01-31 01:50:45 +0200

    fix: php-cs-fixer not ran in /tests

--- tests/AddTest.php
+++ tests/AddTest.php
@@ -3,6 +3,6 @@
 use PhpCsFixer\RuleSet\RuleSets;
 
 test('added a ruleset', function () {
-    expect(RuleSets::getSetDefinitionNames())->toContain('@TenantCloud');
-    expect(RuleSets::getSetDefinition('@TenantCloud'))->not()->toBeNull();
+	expect(RuleSets::getSetDefinitionNames())->toContain('@TenantCloud');
+	expect(RuleSets::getSetDefinition('@TenantCloud'))->not()->toBeNull();
 });
