0fec7d06e838347bb1b0673f4cc7d92e76205cb6
oprypkhantc <54406427+oprypkhantc@users.noreply.github.com>
2024-04-24 23:02:13 +0000

    chore: Sync changes from the template

--- .github/template-sync/replace-bootstrap-in-cs-fixer.js
+++ .github/template-sync/replace-bootstrap-in-cs-fixer.js
@@ -1,9 +1,14 @@
-const {readFile, writeFile} = require("fs/promises");
+const { readFile, writeFile } = require('fs/promises');
 
 module.exports = () => async (template, source) => {
-	const sourceCsFixerConfig = (await readFile(source.path('.php-cs-fixer.dist.php')))
+	const sourceCsFixerConfig = (
+		await readFile(source.path('.php-cs-fixer.dist.php'))
+	)
 		.toString()
-		.replace('vendor/tenantcloud/php-cs-fixer-rule-sets/bootstrap.php', 'bootstrap.php');
+		.replace(
+			'vendor/tenantcloud/php-cs-fixer-rule-sets/bootstrap.php',
+			'bootstrap.php'
+		);
 
 	await writeFile(source.path('.php-cs-fixer.dist.php'), sourceCsFixerConfig);
 
